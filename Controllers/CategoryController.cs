2e4ffa23c62c6933d683aafbe9472d960a95ce79
Thisara Hettikankanam <thisarayashodagcc@gmail.com>
2023-11-01 08:00:52 +0530

    Update CategoryController.cs

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly MongoDBservice _mongoDBservice;
+
+        public CategoryController(MongoDBservice mongoDBservice)
+        {
+            _mongoDBservice = mongoDBservice;
+        }
+
         public IActionResult Index()
         {
+
             return View();
         }
     }
